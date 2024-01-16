53ca6064044ce74387c3043371f02f6eec90a6e2
Amene Terhemen <ameneterh@gmail.com>
2024-01-16 01:34:58 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
