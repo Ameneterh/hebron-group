e7f2b5cb2d40c8af4673f54d22870242b3b646e0
Amene Terhemen <ameneterh@gmail.com>
2024-01-16 08:43:12 +0100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import path from "path";
 
 dotenv.config();
 
@@ -12,8 +15,20 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
+
 const app = express();
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
