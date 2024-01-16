77f6e9140c601f85b7e104daeac1d4d4c25e0057
Amene Terhemen <ameneterh@gmail.com>
2024-01-16 19:28:49 +0100

    complete login page functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,50 @@
-import { Button } from "flowbite-react";
-import React from "react";
+import { Button, Spinner } from "flowbite-react";
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [formData, setFormData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="min-h-screen p-2 sm:p-20">
       <div className="w-full max-w-lg mx-auto">
         <h1 className="text-3xl font-semibold w-full text-center mb-5">
           Login
         </h1>
-        <form className="flex flex-col gap-5">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5">
           <div className="flex flex-col">
             <label for="email" className="w-fit z-10 px-2 bg-white ml-2">
               Your Email
@@ -20,6 +55,7 @@ export default function Login() {
               id="email"
               placeholder="e.g. name@company.com"
               className="rounded-lg mt-[-10px]"
+              onChange={handleChange}
             />
           </div>
           <div className="flex flex-col">
@@ -31,10 +67,13 @@ export default function Login() {
               name="password"
               id="password"
               className="rounded-lg mt-[-10px]"
+              onChange={handleChange}
             />
           </div>
 
-          <Button className="uppercase mt-8">log in</Button>
+          <Button className="uppercase mt-8" type="submit">
+            log in
+          </Button>
         </form>
         <p className="mt-3">
           Do not have an account?{" "}
